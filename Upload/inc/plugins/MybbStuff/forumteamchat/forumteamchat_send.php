8b0022105c7a073055bed7c048e2beffc0ed51bf
Sunil Baral <snlbaral@gmail.com>
2020-12-30 17:05:04 +0545

    mybb friendly code improved

--- Upload/inc/plugins/MybbStuff/forumteamchat/forumteamchat_send.php
+++ Upload/inc/plugins/MybbStuff/forumteamchat/forumteamchat_send.php
@@ -3,17 +3,14 @@ define("IN_MYBB", 1);
 require("../../../../global.php");
 $user = $mybb->user;
 $username = $user['username'];
-$avatar = $user['avatar'];
+//$avatar = $user['avatar'];
 $gid = $user['usergroup'];
 $uid = $user['uid'];
 $curr = date("Y-m-d H:i:s");
 $endtime = date('Y-m-d H:i:s', strtotime($curr. ' + 5 days'));
-if(isset($_POST) && $_POST['msg']!=NULL && $_POST['msg']!=="") {
-	$msg = $_POST['msg'];
+if(isset($mybb->input['msg']) && $mybb->input['msg']!=NULL && $mybb->input['msg']!=="") {
+	$msg = $mybb->input['msg'];
 	$msg = trim(htmlentities(strip_tags($msg)));
-	$unq = rand (00 , 99 );
-	$username = $username;
-	$username = $username;
 	$sql = array(
 		'uid' => $uid,
 		'username' => $username,
